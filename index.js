e96eb029312fab47d44f088431fcf9a15d3ba5c6
Burhaann <burhaann321@gmail.com>
2023-11-30 15:19:07 +0530

    first commit

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.post("/api/shorturl", function (req, res) {
+  const url = req.body;
+  console.log(url);
+  // response = {
+  //   original_url :
+  //   short_url :
+  // }
+  // res.json(response);
+});
+
+app.post("/api/shorturl/:shorturl", function (req, res) {});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
